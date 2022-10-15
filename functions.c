9a24456885674ec5533a307d1c9132dfb807fa5f
DriSidibe <dsidibe653@gmail.com>
2022-10-14 17:11:26 -0700

    commit

---
+++ functions.c
@@ -0,0 +1,11 @@
+/**
+ * _printf - produces output according to a format.
+ * @format: the output format
+ * Return: the number of characters printed 
+*/
+int _printf(const char *format, ...)
+{
+
+
+	return (0);
+}
