34f22b52d7d236431f328f9c47d7e3ceefca6374
DriSidibe <dsidibe653@gmail.com>
2022-10-18 10:54:18 +0000

    fix some bug

--- fonctions.c
+++ fonctions.c
@@ -1,5 +1,30 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
+
+/**
+ * _strlen - calculate the lenght of a str
+ * @s: the string
+ * Return: the lenght og s
+*/
+int _strlen(const char *s)
+{
+    int len;
+
+    for (len = 0; s[len] != '\0'; len++)
+        ;
+    
+    return len;
+}
+
+/**
+ * _print_str - print a string
+ * @s: the string to print
+ * @len: the lenght of s
+*/
+void _print_str(char *s, int len)
+{
+    write(1, s, len);   
+}
 
 /**
  * _calculate_str_args_len - calculate the number of '%' in a str
