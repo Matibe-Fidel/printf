59531841a99212a5e99182ea448de55a46179831
DriSidibe <dsidibe653@gmail.com>
2022-10-18 08:52:20 +0000

    add _strlen and _print_str

--- fonctions.c
+++ fonctions.c
@@ -5,6 +5,31 @@
 #include "main.h"
 
 /**
+ * _strlen - calculate the lenght of a str
+ * @s: the string
+ * Return: the lenght og s
+*/
+int _strlen(const char *s)
+{
+    int len;
+
+    for (len = 0; s[len] != '\0'; len++)
+        ;
+    
+    return len;
+}
+
+/**
+ * _print_str - print a string
+ * @s: the string to print
+ * @len: the lenght of s
+*/
+void _print_str(char *s, int len)
+{
+    write(1, s, len);   
+}
+
+/**
  * _calculate_str_args_len - calculate the number of '%' in a str
  * @s: the string
  * @len: the lenght of s
